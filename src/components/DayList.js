12e3a83d53869820de18dea8c0b1dfa7bf16841c
ryan kang <ryan.kang@clutter.com>
2021-02-15 19:20:56 -0500

    set up application with new components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -8,11 +8,13 @@ import DayListItem from '../components/DayListItem';
 function CreateDayList(props) {
   const days = props.days.map(day => {
     return (
-      <DayListItem 
+      <DayListItem
+        key={day.id} 
         name={day.name} 
         spots={day.spots} 
         selected={day.name === props.day}
-        setDay={props.setDay}  />
+        setDay={props.setDay}  
+      />
     );
   });
 
