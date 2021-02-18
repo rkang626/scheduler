ed7f33b8808b9af2d3de58b68f1ecc610f93847b
ryan kang <ryan.kang@clutter.com>
2021-02-18 08:03:15 -0500

    create hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (replace) {
+      history[history.length - 1] = newMode;
+      setHistory(history);
+    } else {
+      setHistory([...history, newMode]);
+    }
+  };
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      history.pop();
+      setHistory(history);
+    }
+  };
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
